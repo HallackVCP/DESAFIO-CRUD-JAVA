b6ba5a685cc6a7b2340f43ab1337b4d167bc3c27
Vinicius Hallack <viniciushallack@gmail.com>
2021-05-27 11:51:32 -0300

    Criando domain

---
+++ src/main/java/br/com/desafio/api/domain/Cliente.java
@@ -0,0 +1,108 @@
+package br.com.desafio.api.domain;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	Integer id;
+	
+	String nome;
+	
+	Date dataNascimento;
+	
+	char sexo;
+	
+	Integer idade;
+	
+	Cidade cidade;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Integer id, String nome, Date dataNascimento, char sexo, Integer idade, Cidade cidade) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.sexo = sexo;
+		this.idade = idade;
+		this.cidade = cidade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
